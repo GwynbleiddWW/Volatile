b400500a57eba799fea5d308f17554e508370361
Konstantin Shebalin <championacolironni@gmail.com>
2021-06-08 01:26:23 +0700

    Done''

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -4,26 +4,34 @@ public class Main {
     public static void main(String[] args) throws Exception {
         final int sleepTimeout = 3000;
         Tumbler tumbler = new Tumbler();
+        final int counter = 5;
 
         new MyThread(tumbler) {
             public void run() {
                 tumbler.setTurnOn(false);
-                    if (!tumbler.isTurnOn()) {
+                if (!tumbler.isTurnOn()) {
+                    for (int i = 0; i < counter; i++) {
                         System.out.println("Хочу открыть коробку! Включаю тумблер! ");
+                        try {
+                            Thread.sleep(sleepTimeout);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
+            }
         }.start();
 
         new MyThread(tumbler) {
             public void run() {
-                try {
-                    Thread.sleep(sleepTimeout);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 tumbler.setTurnOn(true);
                 if (tumbler.isTurnOn()) {
                     System.out.println("Коробка открылась, закрываю!");
+                    try {
+                        Thread.sleep(sleepTimeout);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }.start();
